6117e2b5ab6c511faa8908848c433837acd6f328
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-05-06 14:13:28 +0500

    in this arrays are discussed

---
+++ arrays-1.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <stdlib.h>
+
+// Arrays
+
+using namespace std;
+main()
+{
+    // example of array
+
+    // int c[100];
+    // int z, i = 0;
+    // do
+    // {
+    //     cin >> z;
+    //     if (z != -1)
+    //         c[i] = z;
+    //     i++;
+
+    // } while (z != -1 && i < 100);
+    // cout << "The total numbers of positive integers entered by the user is " << i - 1;
+
+    // Another example of arrays
+
+    // int a[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; // Initialize the array with some values
+    // int arraySize = 10;
+    // int sumOfSquare = 0;
+
+    // for (int i = 0; i < arraySize; i++)
+    // {
+    //     sumOfSquare += a[i] * a[i];
+    // }
+
+    // cout << "Sum of squares of numbers is " << sumOfSquare << endl;
+
+    // return 0;
+
+    // Another example if array in which we found the given number from user
+
+    // int z;
+    // int a[100], i, found = 0;
+    // for (i = 0; i < 100; i++)
+    // {
+    //     a[i] = i;
+    // }
+
+    // cout << "Please Enter a positive enteger: ";
+    // cin >> z;
+    // for (i = 0; i < 100; i++)
+    // {
+    //     if (z == a[i])
+    //     {
+    //         found = 1;
+    //         break;
+    //     }
+    // }
+    // if (found == 1)
+    // {
+    //     cout << "we found the integer at position " << i;
+    // }
+    // else
+    // {
+    //     cout << "The number was not found";
+    // }
+
+      
+}
