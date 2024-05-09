7afe96a7f802d061e5c774f19801d992b763bd37
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-05-09 17:44:51 +0500

    Add Function

--- simpleCalc.cpp
+++ simpleCalc.cpp
@@ -3,7 +3,14 @@
 
 using namespace std;
 
-
+double inputTrue(double num2, char op)
+{
+    if ((op == '/' || op == '%') && num2 == 0)
+    {
+        cout << "Error! Number can't be divided by zero ";
+        return 0;
+    }
+}
 int main()
 {
     double num1, num2, result;
