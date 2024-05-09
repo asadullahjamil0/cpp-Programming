f440a3b77e654731727c8d4697c4dca4c2059f1b
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-05-09 17:44:21 +0500

    declared variable

--- simpleCalc.cpp
+++ simpleCalc.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 #include <cmath>
 
+using namespace std;
+
+
 int main()
 {
+    double num1, num2, result;
+    char op;
+    cout << "Enter Number 1: ";
+    cin >> num1;
+    cout << "Enter Operator (+, -, /, *): ";
+    cin >> op;
+    cout << "Enter Number 2: ";
+    cin >> num2;
+
     
 }
