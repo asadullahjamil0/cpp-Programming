ed5aeedad2d3ca62f2edd9bcfd7ac16733fc4419
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-05-11 23:26:07 +0500

    Add Variables and loops

--- project1.cpp
+++ project1.cpp
@@ -2,22 +2,66 @@
 using namespace std;
 
 bool gameOver;
+const int width = 20;
+const int height = 20;
+int x, y, fruitX, fruitY, score;
+enum eDirection
+{
+    STOP = 0,
+    LEFT,
+    RIGHT,
+    UP,
+    DOWN
+};
+eDirection dir;
+
 void Setup()
 {
     gameOver = false;
+    dir = STOP;
+    x = width / 2;
+    x = height / 2;
+    fruitX = rand() % width;
+    fruitY = rand() % height;
+    score = 0;
 }
 void Draw()
 {
-    
+    system("cls"); // system clear
+    for (int i = 0; i < width + 2; i++)
+    {
+        cout << "#";
+    }
+    cout << endl;
 
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < height; j++)
+        {
+            if (j == 0)
+            {
+                cout << "#";
+                cout << "";
+            }
+            if (j == width - 1)
+            {
+                cout << "#";
+            }
+            cout << endl;
+        }
+    }
+
+    for (int i = 0; i < width +2; i++)
+    {
+        cout << "#";
+    }
+    cout << endl;
 }
 void Input()
 {
-
 }
 void Logic()
 {
-
 }
 
 int main()
@@ -29,6 +73,5 @@ int main()
         Input();
         Logic();
     }
-    
-
+    return 0;
 }
