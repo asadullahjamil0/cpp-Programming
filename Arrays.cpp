d53f8824664c3724e98c7d93e62516fe5838d326
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-05-14 19:51:23 +0500

    Arrays

---
+++ Arrays.cpp
@@ -0,0 +1,29 @@
+
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    // array example
+    int marks[4] = {23, 45, 56, 89};
+    int mathMarks[4];
+    mathMarks[0] = 234;
+    mathMarks[1] = 234;
+    mathMarks[2] = 234;
+    mathMarks[3] = 234;
+    cout << "these are math marks";
+    cout << mathMarks[0] << endl;
+    cout << mathMarks[1] << endl;
+    cout << mathMarks[2] << endl;
+    cout << mathMarks[3] << endl;
+    cout << "these are marks" << endl;
+
+    // You can change the value of
+    marks[0] = 234;
+    cout << marks[0] << endl;
+    cout << marks[1] << endl;
+    cout << marks[2] << endl;
+    cout << marks[3] << endl;
+
+    return 0;
+}
