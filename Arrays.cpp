f598178b636c2d5719227b38960ac6a0f9a71283
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-05-14 20:22:37 +0500

    Pointers in c++

--- Arrays.cpp
+++ Arrays.cpp
@@ -20,13 +20,13 @@ int main()
     cout << "these are marks" << endl;
 
     // You can change the value of
-    marks[0] = 234;
+    marks[0] = 276;
     // cout << marks[0] << endl;
     // cout << marks[1] << endl;
     // cout << marks[2] << endl;
     // cout << marks[3] << endl;
 
-    cout << "This is from for loop"<<endl;
+    cout << "This is from for loop" << endl;
     for (int i = 0; i < 4; i++)
     {
         cout << "the value of marks " << i << " is " << marks[i] << endl;
@@ -49,5 +49,16 @@ int main()
         i++;
     } while (i < 4);
 
+    // Pointers and Arrays
+
+    int *p = marks;
+    cout << "the value of marks[0] is " << *(p++) << endl;
+    cout << "the value of marks[1] is " << *(p++) << endl;
+    cout << "the value of marks[2] is " << *(p++) << endl;
+    cout << "the value of marks[3] is " << *(p++) << endl;
+    // cout << "the value of *P is " << *p << endl;
+    // cout << "the value of *(p+1) is " << *(p+1) << endl;
+    // cout << "the value of *(p+2) is " << *(p+2) << endl;
+    // cout << "the value of *(p+3) is " << *(p+3) << endl;
     return 0;
 }
