7b356a722603204a013d10b1ab064e0c83074c1c
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-05-14 19:56:50 +0500

    Printing value by loop

--- Arrays.cpp
+++ Arrays.cpp
@@ -21,10 +21,15 @@ int main()
 
     // You can change the value of
     marks[0] = 234;
-    cout << marks[0] << endl;
-    cout << marks[1] << endl;
-    cout << marks[2] << endl;
-    cout << marks[3] << endl;
+    // cout << marks[0] << endl;
+    // cout << marks[1] << endl;
+    // cout << marks[2] << endl;
+    // cout << marks[3] << endl;
+
+    for (size_t i = 0; i < 4; i++)
+    {
+        cout << "the value of marks " << i << " is " << marks[i] << endl;
+    }
 
     return 0;
 }
