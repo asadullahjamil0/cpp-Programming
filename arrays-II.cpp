9034a19e3ab8540855b1c4b0a2571ffbbb44a167
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-05-16 11:49:01 +0500

    For both row and column reverse matrix

--- arrays-II.cpp
+++ arrays-II.cpp
@@ -48,5 +48,16 @@ int main()
         cout << endl;
     }
 
+    // for both row and column reverse matrix
+    cout << "The reverse column matrix is: " << endl;
+    for (row = maxRows - 1; row >= 0; row--)
+    {
+        for (col = maxCols - 1; col >= 0; col--)
+        {
+            cout << a[row][col] << '\t';
+        }
+        cout << endl;
+    }
+
     return 0;
 }
