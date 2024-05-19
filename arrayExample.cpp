0edeb609ddda6ad38047ae100658879747901448
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-05-19 13:47:31 +0500

    Did some code

---
+++ arrayExample.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int numEmps;
+cout << "Enter the number of employees: ";
+cin >> numEmps;
+void getInput(int[][2], int numEps)
+{
+    for ( i = 0; i < numEps; i++)
+    {
+        cin>>sal[i][o];
+    }
+    
+
+}
+int main()
+{
+    const int arraySize = 100;
+    int sal[arraySize][2];
+    int lucky[arraySize] = {0};
+    getInput(sal, numEmps); 
+}
