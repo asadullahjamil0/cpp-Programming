7667a794be20dd1cd2841f06924c57675f9028ae
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-05-25 23:41:47 +0500

    Another example of pointers

--- pointersInC++.cpp
+++ pointersInC++.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int number = 42; // Declare an integer variable
+int main()
+{
+    int number = 42;        // Declare an integer variable
     int *pointer = &number; // Declare a pointer and assign it the address of 'number'
 
     // Output the value of the variable
@@ -16,6 +17,20 @@ int main() {
 
     // Output the address stored in the pointer
     cout << "Address stored in pointer: " << pointer << endl;
+
+    // Another example
+    int cat = 30;
+    string catName = "Raani";
+
+    int *storeCat = &cat;
+    string *storeCatName = &catName;
+
+    cout << "Number of my cat is: " << cat << '\n';
+    cout << "Name of my cat is: " << catName << '\n';
+
+    cout << "Number's Address  of my cat is: " << &cat << '\n';
+    cout << "Name's Address  of my cat is: " << &catName << '\n';
+    return 0;
 
     return 0;
 }
