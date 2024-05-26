6026a127dd3a24bbab202fe35910114af8de7a78
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-05-26 20:28:48 +0500

    finding address by for loop

--- arraysPointers.cpp
+++ arraysPointers.cpp
@@ -16,4 +16,11 @@ int main()
     cout << "Accessing the 6th element of array as y[5]= " << y[5] << endl;
     cout << "Accessing the 6th element of array as yptr[5]= " << yptr[4] << endl;
     cout << "Accessing the 6th element of array as *(yptr + 5)= " << *(yptr + 4) << endl;
+
+    // Finding address by for loop
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "The address of element at position: " << i << " " << yptr << endl;
+        yptr++;
+    }
 }
