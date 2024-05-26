4e478f0166ab39cd07b202e38251fcbbe5a69295
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-05-26 20:25:02 +0500

    Assigning the address of elements

--- arraysPointers.cpp
+++ arraysPointers.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 
 using namespace std;
-int main ()
+int main()
 {
-    int y [10];
+    int y[10];
     int *yptr;
     yptr = y;
-    
-   
-    cout<<"The memory address of YPTR is: "<<yptr<<endl;
-    yptr ++;
-    cout<<"The memory address of YPTR is: "<<yptr<<endl;
 
+    cout << "The memory address of YPTR is: " << yptr << endl;
+    yptr++;
+    cout << "The memory address of YPTR is: " << yptr << endl;
+
+    // Assigning the address of elements
+
+    cout << "Accessing the 6th element of array as y[5]= " << y[5] << endl;
+    cout << "Accessing the 6th element of array as yptr[5]= " << yptr[4] << endl;
+    cout << "Accessing the 6th element of array as *(yptr + 5)= " << *(yptr + 4) << endl;
 }
