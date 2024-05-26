7896cb15a21145ffae9595a7dea61fe4ca9966cc
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-05-26 20:15:20 +0500

    Size of data types

---
+++ sizeOfDataType.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    cout << "the size of data type int is: " << sizeof(int) << endl;
+    cout << "the size of data type float is: " << sizeof(float) << endl;
+    cout << "the size of data type char is: " << sizeof(char) << endl;
+    cout << "the size of data type string is: " << sizeof(string) << endl;
+    cout << "the size of data type double is: " << sizeof(double) << endl;
+    cout << "the size of data type long is: " << sizeof(long) << endl;
+    cout << "the size of data type short is: " << sizeof(short) << endl;
+    return 0;
+}
