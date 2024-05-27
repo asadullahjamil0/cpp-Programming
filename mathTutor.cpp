35a7aca77371d4bb17594bd0ed05d35ddc4b171d
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-05-27 23:01:37 +0500

    get userTotal form user and then print relevant choice to the screen according to the user's choice

--- mathTutor.cpp
+++ mathTutor.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <ctime>
 #include <iomanip>
-
+#include <cstdlib>
 
 using namespace std;
-int main ()
+
+int main()
 {
     int randNum1, randNum2;
     int userTotal, computerTotal;
@@ -15,8 +16,35 @@ int main ()
     // seed into random number generator
     srand(seed);
 
+    // generate two random numbers between 1 and 100
     randNum1 = 1 + rand() % 100;
     randNum2 = 1 + rand() % 100;
 
+    // calculate the computer's total
     computerTotal = randNum1 + randNum2;
+
+    // display the first random number
+    cout << setw(5) << randNum1 << endl;
+
+    // display the second random number
+    cout << setw(3) << "+" << randNum2 << endl;
+
+    // display the separator line
+    cout << "---------------" << endl;
+
+    // prompt the user to enter the total
+    cout << "Please Enter your total: ";
+    cin >> userTotal;
+
+    // check if the user's total matches the computer's total
+    if (userTotal == computerTotal)
+    {
+        cout << "Congratulations, you calculated the right answer" << endl;
+    }
+    else
+    {
+        cout << "Sorry! Wrong answer. Computer's total is: " << computerTotal << endl;
+    }
+
+    return 0;
 }
