632f34672111c4fd1ac17951e8687797674fb84f
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-05-29 22:21:53 +0500

    Minor change

--- oceanLevelRising.cpp
+++ oceanLevelRising.cpp
@@ -3,10 +3,12 @@
 using namespace std;
 int main()
 {
+    // Declaring variables
     float oceanLevelRising;
     float risePerYear = 1.5;
+    int years = 25;
 
-    for (int i = 1; i <= 25; i++)
+    for (int i = 1; i <= years; i++)
     {
         oceanLevelRising = i * risePerYear;
         cout << "Ocean rised " << oceanLevelRising << "mm " << " at year " << i << endl;
