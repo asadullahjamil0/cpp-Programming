98e6515f338c740956737743456fe26ec7233dfd
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-06-12 22:57:44 +0500

    daily salary double calclulator

---
+++ dailySalaryCalculator.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    int numberOfDays;
+    double dailySalary = 0.01;
+    double totalSalary = 0.0;
+
+    cout << "Please enter the number of days you worked: ";
+    cin >> numberOfDays;
+
+    for (int i = 1; i < numberOfDays; i++) {
+        totalSalary += dailySalary;
+        cout << "Day " << i << ": $" << fixed << setprecision(2) << dailySalary << endl;
+        dailySalary *= 2;
+    }
+
+    cout << "Total salary for " << numberOfDays << " days is $" << fixed << setprecision(2) << totalSalary << "." << endl;
+
+    return 0;
+}
