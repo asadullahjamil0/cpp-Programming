de39c3c4728a159ea055f2398304a4d11db1dcb7
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-06-16 15:01:00 +0500

    data hidding in encapsulation

--- oops.cpp
+++ oops.cpp
@@ -29,15 +29,26 @@ public:
     }
 };
 
+class account
+{
+private:
+    string password;
+    double salary;
+
+public:
+    string accountId;
+    string username;
+};
+
 int main()
 {
     Teacher t1;
     t1.name = "AsadUllah";
     t1.subject = "C++";
     t1.dept = "Computer Science";
-    t1.setSalary (75000);
+    t1.setSalary(75000);
 
-    cout <<"Name: "<< t1.name << endl;
-    cout <<"Salary: "<< t1.getSalary() << endl;
+    cout << "Name:  " << t1.name << endl;
+    cout << "Salary: " << t1.getSalary() << endl;
     return 0;
 }
