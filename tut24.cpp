f5d4b36e375fab3a3e98b06257842f99983534ab
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-06-22 15:38:25 +0500

    Static member function

--- tut24.cpp
+++ tut24.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 using namespace std;
 
-// Static data members
 class Employee
 {
     int id;
@@ -20,26 +19,35 @@ public:
         cout << "Id of this employee is: " << id << " and this is employee no: " << count << endl;
         cout << "Name of this employee is: " << name << endl;
     }
+    static void getCount(void)
+    {
+        // cout << id; Through an error
+        cout << "The value of count is: " << count << endl;
+    }
 };
 
 int Employee ::count; /* Default value of static variable is 0*/
 int main()
 {
     Employee asad, noman, ijaz;
+    // count is a Static data members
     // asad.id = 1;
     // asad.count = 1; cannot do this as id and count are privates
 
     asad.name = "AsadUllah";
     asad.setData();
     asad.getData();
+    Employee ::getCount();
 
     noman.name = "Nouman";
     noman.setData();
     noman.getData();
+    Employee ::getCount();
 
     ijaz.name = "Ijaz";
     ijaz.setData();
     ijaz.getData();
+    Employee ::getCount();
 
     return 0;
 }
