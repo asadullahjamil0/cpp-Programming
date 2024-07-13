1880c96383e4eff91f646853cedd30de6daf0fc1
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-07-13 22:02:15 +0500

    Empty function in c++ and function prototype.

--- tut15.cpp
+++ tut15.cpp
@@ -2,7 +2,11 @@
 using namespace std;
 
 // Function prototype
+// Type function-name(Arguements);
 int sum(int a, int b);
+
+// Empty/Void function
+void g(void);
 int main()
 {
     int num1, num2;
@@ -10,13 +14,22 @@ int main()
     cin >> num1;
     cout << "Enter your first number: ";
     cin >> num2;
+
+    // num1 and num2 are actual parameters
     cout << "Sum of your two numbers is: " << sum(num1, num2) << endl;
+    g();
 
     return 0;
 }
 
 int sum(int a, int b)
 {
+    // Formal paramters a and b takes value from actual paramteres
     int c = a + b;
     return c;
+}
+
+void g()
+{
+    cout << "Hello this is empty function";
 }
