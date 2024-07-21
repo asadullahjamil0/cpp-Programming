e9f896e4be35364d178dc58a630f5c3306581fa0
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-07-21 17:22:41 +0500

    More deeply in file handling in c++.

---
+++ fileHandlingIII.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main()
+{
+    string data;
+    ifstream read;
+    read.open("D:/New folder (2)/practise.txt", ios::in);
+
+    if (!read.is_open())
+    {
+        cout << "Error file opening";
+        return 1;
+    }
+    else
+    {
+        read.seekg(5, ios::beg);
+        while (!read.eof())
+        {
+            data = read.get();
+            // getline(read,data);
+            cout << data;
+        }
+    }
+
+    return 0;
+}
