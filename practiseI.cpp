400fe5dfa7768a56ec387e18b01fb115c9218c94
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-07-22 13:53:31 +0500

    practise code of constructors in c++.

--- practiseI.cpp
+++ practiseI.cpp
@@ -1,28 +1,56 @@
+// #include <iostream>
+// using namespace std;
+
+// class Salary
+// {
+//     int salary;
+
+// public:
+//     void set()
+//     {
+//         system("cls");
+//         cout << "Salary Loading...." << endl;
+//         salary = 50000;
+//     }
+//     void disalySal()
+//     {
+//         cout << "Your current salary is: " << salary << endl;
+//     }
+// };
+
+// int main()
+// {
+//     Salary s1;
+//     s1.set();
+//     s1.disalySal();
+
+//     return 0;
+// }
+
 #include <iostream>
 using namespace std;
 
-class Salary
+class Calculator
 {
-    int salary;
+    float number1;
+    float number2;
 
 public:
-    void set()
+    Calculator(float num1, float num2)
     {
-        system("cls");
-        cout << "Salary Loading...." << endl;
-        salary = 50000;
+        number1 = num1;
+        number2 = num2;
     }
-    void disalySal()
+    void display()
     {
-        cout << "Your current salary is: " << salary << endl;
+        cout << "Product of your numbers is: " << number1 * number2 << endl;
     }
 };
 
 int main()
 {
-    Salary s1;
-    s1.set();
-    s1.disalySal();
+    Calculator c1(100, 50);
+    c1.display();
 
     return 0;
 }
