f1ae3daf3e8fe653bcc048c7665f0a94657e9e92
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-09-24 13:53:44 +0500

    Basic square pattern in c++

--- patterns in c++/patterns.cpp
+++ patterns in c++/patterns.cpp
@@ -3,11 +3,38 @@ using namespace std;
 
 int main()
 {
-    for (int i = 1; i <= 4; i++)
+    // Square pattern in the form of numbers,star and charcter to print
+    int n;
+    cout << "Enter number for print:";
+    cin >> n;
+    // in the form of number
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 1; j <= 4; j++)
+        for (int j = 0; j < n; j++)
         {
-            cout << j;
+            cout << j + 1;
+        }
+        cout << endl;
+    }
+
+    // in the form of stars
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    // In the form of character
+    for (int i = 0; i < n; i++)
+    {
+        char ch = 'A';
+        for (int j = 0; j < n; j++)
+        {
+            cout << ch;
+            ch++;
         }
         cout << endl;
     }
