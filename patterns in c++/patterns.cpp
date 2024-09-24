e511c3eec831bb8351c55d9ee8dd4ea7d3647e29
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-09-24 15:04:44 +0500

    Triangle pattern

--- patterns in c++/patterns.cpp
+++ patterns in c++/patterns.cpp
@@ -8,34 +8,57 @@ int main()
     cout << "Enter number for print:";
     cin >> n;
     // in the form of number
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            cout << j + 1;
-        }
-        cout << endl;
-    }
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < n; j++)
+    //     {
+    //         cout << j + 1;
+    //     }
+    //     cout << endl;
+    // }
 
     // in the form of stars
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < n; j++)
+    //     {
+    //         cout << "*";
+    //     }
+    //     cout << endl;
+    // }
+
+    // In the form of character
+    // for (int i = 0; i < n; i++)
+    // {
+    //     char ch = 'A';
+    //     for (int j = 0; j < n; j++)
+    //     {
+    //         cout << ch;
+    //         ch++;
+    //     }
+    //     cout << endl;
+    // }
+
+    // int num = 1;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < n; j++)
+    //     {
+    //         cout << num <<" ";
+    //         num++;
+    //     }
+    //     cout << endl;
+    // }
+
+    // Triangle Pattern
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n; j++)
+        for (int j = 0; j < i + 1; j++)
         {
             cout << "*";
         }
         cout << endl;
     }
 
-    // In the form of character
-    for (int i = 0; i < n; i++)
-    {
-        char ch = 'A';
-        for (int j = 0; j < n; j++)
-        {
-            cout << ch;
-            ch++;
-        }
-        cout << endl;
-    }
+    return 0;
 }
