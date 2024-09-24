e13071a0dd4812a20cec7e06a35451105936c6ce
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-09-24 16:10:01 +0500

    Floyd's triangle pattern

--- patterns in c++/patterns.cpp
+++ patterns in c++/patterns.cpp
@@ -50,12 +50,70 @@ int main()
     //     cout << endl;
     // }
 
+    // Another
     // Triangle Pattern
+    // For stars
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < i + 1; j++)
+    //     {
+    //         cout << "* ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // for numbers
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < i + 1; j++)
+    //     {
+    //         cout << i + 1;
+    //     }
+    //     cout << endl;
+    // }
+
+    // Another
+    // for number
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < i + 1; j++)
+    //     {
+    //         cout << j + 1;
+    //     }
+    //     cout << endl;
+    // }
+
+    // reverse triangle pattern for number
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = i; j >= 0; j--)
+    //     {
+    //         cout << j +1;
+    //     }
+    //     cout << endl;
+    // }
+
+    // Floyd's triangle pattern
+    // For number
+    // int num = 1;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = i + 1; j > 0; j--)
+    //     {
+    //         cout << num << " ";
+    //         num++;
+    //     }
+    //     cout << endl;
+    // }
+
+    // for character
+    char ch = 'A';
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < i + 1; j++)
+        for (int j = i + 1; j > 0; j--)
         {
-            cout << "*";
+            cout << ch << " ";
+            ch++;
         }
         cout << endl;
     }
