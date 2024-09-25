8a4d905dd9b78e0e205eb6cd62e6c99410b11c89
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-09-25 11:46:09 +0500

    Pyramid triangle

--- patterns in c++/patterns.cpp
+++ patterns in c++/patterns.cpp
@@ -149,5 +149,24 @@ int main()
     //     ch++;
     // }
 
+    // pyramid pattern
+    // for numbers
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 0; j < i + 1; j++)
+        {
+            cout << j + 1;
+        }
+        for (int j = 0; j < i; j++)
+        {
+            cout << j + 1;
+        }
+        cout << endl;
+    }
+
     return 0;
 }
