f10142e5a8a03d0ca845956179a242efb513b207
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-09-29 21:14:12 +0500

    binary to decimal conversion

--- binary number system/main.cpp
+++ binary number system/main.cpp
@@ -16,16 +16,36 @@ int decToBin(int decimalNum)
     return ans;
 }
 
+int binToDec(int binNum)
+{
+    int rem;
+    int pow = 1;
+    int ans = 0;
+    while (binNum > 0)
+    {
+        rem = binNum % 10;
+        binNum = binNum / 10;
+        ans += (rem * pow);
+        pow *= 2;
+    }
+    return ans;
+}
+
 int main()
 {
     // int decimalNum;
     // cout << "Enter decimal number to add binary number";
     // cin >> decimalNum;
     // to print more than one binary number
-    for (int i = 1; i <= 10; i++)
-    {
-        cout << "Binary form of number" << i << " is:" << decToBin(i) << endl;
-    }
+    // for (int i = 1; i <= 10; i++)
+    // {
+    //     cout << "Binary form of number" << i << " is:" << decToBin(i) << endl;
+    // }
+
+    int binNum;
+    cout << "Enter binary number to convert into decimal number:";
+    cin >> binNum;
+    cout << "Decimal number of your provided binary number is:" << binToDec(binNum) << endl;
 
     return 0;
 }
