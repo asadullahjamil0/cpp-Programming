6ee154a6ba55f8d74caaa302238aa1e3b020a177
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-09-30 14:34:00 +0500

    Bitwise operator and data type modifier

---
+++ bitwise operator/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // int a = 3, b = 7;
+    // cout << (a ^ b) << endl;
+    // cout << (6 ^ 10) << endl;
+
+    // left shift operator
+    // cout << (10 << 2) << endl;
+    // right shift operator
+    // cout << (10 >> 1) << endl;
+
+    // Data type modifiers
+    int x;
+    cout << sizeof(x) << endl;
+    cout << sizeof(long long int) << endl;
+    cout << sizeof(short int) << endl;
+
+    // unisigned and signed data-type
+    unsigned int y;
+    cout << "Enter number id:";
+    cin >> y;
+    cout << y;
+    return 0;
+}
