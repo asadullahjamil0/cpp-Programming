a3e8a66f8d97c608677bdf533234de5aea901179
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-10-02 20:38:12 +0500

    Array reversed in c++

--- Arrays/arraysII.cpp
+++ Arrays/arraysII.cpp
@@ -10,17 +10,28 @@ void checkArr(int arr[], int size)
     }
 }
 
-int linearSearch(int arr_1[], int sz)
+int linearSearch(int arr_1[], int sz, int target)
 {
     for (int i = 0; i < sz; i++)
     {
 
-        if (arr_1[i] == 5)
+        if (arr_1[i] == target)
         {
             return i;
         }
     }
     return -1;
+}
+
+// Reverse array function
+void reverseArray(int arr_2[], int start, int end)
+{
+    while (start <= end)
+    {
+        swap(arr_2[start], arr_2[end]);
+        start++;
+        end--;
+    }
 }
 
 int main()
@@ -37,7 +48,20 @@ int main()
 
     // Linear search in array
     int arr_1[] = {2, 3, 4, 5, 7, 8};
-    cout << linearSearch(arr_1, 6) << endl;
+    int target = 7;
+    cout << linearSearch(arr_1, 6, target) << endl;
+
+    // Reversed array
+    int arr_2[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int start = 0;
+    int end = 8;
+
+    reverseArray(arr_2, start, end);
+    cout << "Your reversed array is:" << endl;
+    for (int i = 0; i < 9; i++)
+    {
+        cout << arr_2[i] << " ";
+    }
 
     return 0;
 }
