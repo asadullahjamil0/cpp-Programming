998f2b4c9de38ef0e44a1fd23271a40ba70d8979
Asad Ullah Jamil Ch <161153558+asadullahjamil0@users.noreply.github.com>
2025-05-04 17:50:19 +0100

    Update patterns.cpp
    Simple pattern included for practise.

--- patterns in c++/patterns.cpp
+++ patterns in c++/patterns.cpp
@@ -246,5 +246,17 @@ int main()
         cout << endl;
     }
 
+    // Simple pattern for practise
+    int num;
+    cout << "Enter number of rows: ";
+    cin >> num;
+
+    for (int i = 1; i <= num; i++) {
+        for (int j = 1; j <= num - i; j++)
+            cout << " ";
+        for (int j = 1; j <= 2 * i - 1; j++)
+            cout << "*";
+        cout << endl;
+    }
     return 0;
 }
