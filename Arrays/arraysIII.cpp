f6c3f8edf9a3b64e53de09c5e48c5e909cd98dbf
Asad Ullah Jamil Ch <161153558+asadullahjamil0@users.noreply.github.com>
2025-05-05 18:58:11 +0100

    Update arraysIII.cpp
    Added some code for practise.

--- Arrays/arraysIII.cpp
+++ Arrays/arraysIII.cpp
@@ -8,19 +8,46 @@ int main()
     cout << "Before = " << vec.size() << endl;
     vec.push_back(1);
     cout << "After push_back = " << vec.size() << endl;
-    // vector<int> hel = {1, 2, 3};
-    // cout << hel[0] << endl;
-    // vector<int> wor(4, 3);
+
     vector<char> wor = {'a', 'b', 't', 'g', 'u'};
-    // These are some functions we can perform to vecotr
-    wor.pop_back();
-    cout << "This is for front() " << wor.front() << endl;
-    cout << "This is for back() " << wor.back() << endl;
-    cout << "This is for at() " << wor.at(2) << endl;
+
+    wor.pop_back(); // removes last element
+
+    cout << "First element (front): " << wor.front() << endl;
+    cout << "Last element (back): " << wor.back() << endl;
+    cout << "Element at index 2: " << wor.at(2) << endl;
+
+    cout << "All elements in wor:" << endl;
     for (char val : wor)
     {
-        cout << val << endl;
+        cout << val << " ";
     }
+    cout << endl;
+
+    // NEW CODE: Input vector from user
+    vector<int> numbers;
+    int n;
+    cout << "How many numbers do you want to enter? ";
+    cin >> n;
+
+    cout << "Enter " << n << " numbers:" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        int num;
+        cin >> num;
+        numbers.push_back(num);
+    }
+
+    cout << "You entered: ";
+    for (int val : numbers)
+    {
+        cout << val << " ";
+    }
+    cout << endl;
+
+    // Clear the numbers vector
+    numbers.clear();
+    cout << "After clearing, numbers.size() = " << numbers.size() << endl;
 
     return 0;
 }
